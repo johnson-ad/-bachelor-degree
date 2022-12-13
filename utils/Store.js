c804f255410d840d826a687cc8c237758ac3731a
='Johnson-ad <=>
2022-12-13 16:15:10 +0000

    Fin implementation du choix de moyen de paiment

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
@@ -51,6 +51,14 @@ function reducer(state, action) {
           },
         },
       };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      };
     default:
       return state;
   }
@@ -61,50 +69,3 @@ export function StoreProvider({ children }) {
   const value = { state, dispatch };
   return <Store.Provider value={value}>{children}</Store.Provider>;
 }
-
-// import { createContext, useReducer } from 'react';
-// import Cookies from 'js-cookie';
-
-// export const Store = createContext();
-
-// const initialState = {
-//   cart: Cookies.get('cart')
-//     ? JSON.parse(Cookies.get('cart'))
-//     : { cartItems: [] },
-// };
-
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case 'CART_ADD_ITEM': {
-//       const newItem = action.payload;
-//       const existItem = state.cart.cartItems.find(
-//         (item) => item.slug === newItem.slug
-//       );
-//       const cartItems = existItem
-//         ? state.cart.cartItems.map((item) => {
-//             item.name === existItem.name ? newItem : item;
-//           })
-//         : [...state.cart.cartItems, newItem];
-//       Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
-//       return { ...state, cart: { ...state.cart, cartItems } };
-//     }
-//     case 'CART_REMOVE_ITEM': {
-//       const cartItems = state.cart.cartItems.filter(
-//         (item) => item.slug !== action.payload.slug
-//       );
-//       Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
-//       return { ...state, cart: { ...state.cart, cartItems } };
-//     }
-//     // case 'CART_ADD_ITEM': {
-//     //   return;
-//     // }
-//     default:
-//       return state;
-//   }
-// }
-
-// export function StoreProvider({ children }) {
-//   const [state, dispatch] = useReducer(reducer, initialState);
-//   const value = { state, dispatch };
-//   return <Store.Provider value={value}>{children}</Store.Provider>;
-// }

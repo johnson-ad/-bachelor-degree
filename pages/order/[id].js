6a8a72344093fe34c6f76fbf8722879e11331578
='Johnson-ad <=>
2022-12-21 22:38:50 +0000

    Fin creation de la page order ou commande en fr

---
+++ pages/order/[id].js
@@ -0,0 +1,202 @@
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useReducer } from 'react';
+import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+import { motion } from 'framer-motion';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, order: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+function OrderScreen() {
+  // order/:id
+  const { query } = useRouter();
+  const orderId = query.id;
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders/${orderId}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+  }, [order, orderId]);
+  const {
+    shippingAddress,
+    paymentMethod,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt,
+  } = order;
+
+  return (
+    <Layout title={`Order ${orderId}`}>
+      <motion.h1
+        animate={{ opacity: 1, y: 5 }}
+        transition={{ delay: 0.6 }}
+        initial={{ opacity: 0, y: 0 }}
+        className="mb-5 text-xl"
+      >{`Order ${orderId}`}</motion.h1>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div className="alert-error">{error}</div>
+      ) : (
+        <motion.div
+          animate={{ opacity: 1, y: 5 }}
+          transition={{ type: 'spring', delay: 0.8 }}
+          initial={{ opacity: 0, y: 0 }}
+          className="grid md:grid-cols-4 md:gap-5"
+        >
+          <div className="overflow-x-auto md:col-span-3">
+            <motion.div
+              animate={{ opacity: 1, y: 2 }}
+              transition={{ type: 'spring', delay: 1.2 }}
+              initial={{ opacity: 0, y: 0 }}
+              className="card  p-5"
+            >
+              <h2 className="mb-2 text-lg">Shipping Address</h2>
+              <div>
+                {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                {shippingAddress.city}, {shippingAddress.postalCode},{' '}
+                {shippingAddress.country}
+              </div>
+              {isDelivered ? (
+                <div className="alert-success">Delivered at {deliveredAt}</div>
+              ) : (
+                <div className="alert-error">Not delivered</div>
+              )}
+            </motion.div>
+
+            <motion.div
+              animate={{ opacity: 1, y: 1 }}
+              transition={{ type: 'spring', delay: 1.5 }}
+              initial={{ opacity: 0, y: 0 }}
+              className="card p-5"
+            >
+              <h2 className="mb-2 text-lg">Payment Method</h2>
+              <div>{paymentMethod}</div>
+              {isPaid ? (
+                <div className="alert-success">Paid at {paidAt}</div>
+              ) : (
+                <div className="alert-error">Not paid</div>
+              )}
+            </motion.div>
+
+            <motion.div
+              animate={{ opacity: 1, y: 1 }}
+              transition={{ type: 'spring', delay: 1.8 }}
+              initial={{ opacity: 0, y: 0 }}
+              className="card overflow-x-auto p-5"
+            >
+              <h2 className="mb-2 text-lg">Order Items</h2>
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">Item</th>
+                    <th className="    p-5 text-right">Quantity</th>
+                    <th className="  p-5 text-right">Price</th>
+                    <th className="p-5 text-right">Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderItems.map((item) => (
+                    <tr key={item._id} className="border-b">
+                      <td>
+                        <Link href={`/product/${item.slug}`}>
+                          <a className="flex items-center">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                              objectFit="cover"
+                            ></Image>
+                            &nbsp;
+                            {item.name}
+                          </a>
+                        </Link>
+                      </td>
+                      <td className=" p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">
+                        ${item.quantity * item.price}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </motion.div>
+          </div>
+          <div>
+            <motion.div
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: 'spring', delay: 2.4 }}
+              initial={{ opacity: 0, x: -15 }}
+              className="card  p-5"
+            >
+              <h2 className="mb-2 text-lg">Order Summary</h2>
+              <ul>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Items</div>
+                    <div>${itemsPrice}</div>
+                  </div>
+                </li>{' '}
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Tax</div>
+                    <div>${taxPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Shipping</div>
+                    <div>${shippingPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Total</div>
+                    <div>${totalPrice}</div>
+                  </div>
+                </li>
+              </ul>
+            </motion.div>
+          </div>
+        </motion.div>
+      )}
+    </Layout>
+  );
+}
+
+OrderScreen.auth = true;
+export default OrderScreen;
